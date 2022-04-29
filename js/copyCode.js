89b023b473a184b1361bee753d3111aa05475fee
bobyang08250772 <87290044+bobyang08250772@users.noreply.github.com>
2022-04-29 11:07:50 +0800

    Update copyCode.js

--- js/copyCode.js
+++ js/copyCode.js
@@ -3,22 +3,22 @@ const codeBlocks = document.querySelectorAll('.hljs');
 const copyCodeButtons = document.querySelectorAll('.copy-code-button');
 
 copyCodeButtons.forEach((copyCodeButton, index) => {
-  const code = codeBlocks[index].textContent;
+  const code = codeBlocks[index].innerText;
 
   copyCodeButton.addEventListener('click', () => {
     // Copy the code to the user's clipboard
     window.navigator.clipboard.writeText(code);
 
     // Update the button text visually
-    const { textContent: originalText } = copyCodeButton;
-    copyCodeButton.textContent = 'Copied!';
+    const { innerText: originalText } = copyCodeButton;
+    copyCodeButton.innerText = 'Copied!';
 
     // (Optional) Toggle a class for styling the button
     copyCodeButton.classList.add('copied');
 
     // After 2 seconds, reset the button to its initial UI
     setTimeout(() => {
-      copyCodeButton.textContent = originalText;
+      copyCodeButton.innerText = originalText;
       copyCodeButton.classList.remove('copied');
     }, 2000);
   });
